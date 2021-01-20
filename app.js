3056f230ea4d62ed269376aec68a7bcd957786a2
Dawid <Dawid_pacanowski@wp.pl>
2021-01-20 21:22:02 +0100

    simple volume animation + go back button reseting music duration

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const line = document.querySelector(".timeLine");
 const playButton = document.querySelector(".playStop");
 const volume = document.querySelector(".volume-range");
+const speakerIcon = document.querySelector(".speakerIcon");
+const backBtn = document.querySelector(".backButton");
 //dodac zmniejsdzone volume
 class MusicApp {
     constructor(song) {
         this.song = song;
         this.song.onended = () => {
             this.stop();
+            //if no queue
         };
     }
     play() {
@@ -20,6 +23,11 @@ class MusicApp {
         clearInterval(this.stopInterval);
         playButton.src = "/icons/play.svg";
         playButton.setAttribute("name","play");
+    }
+    goBack() {
+        this.song.currentTime = 0;
+        line.style.width = "";
+        this.stop();
     }
     volumeSetting() {
         this.song.volume = volume.value/100;
@@ -41,9 +49,38 @@ playButton.addEventListener('click', () => {
         Player.stop();
     }
 });
+backBtn.addEventListener('click', () => {
+    Player.goBack();
+});
 volume.addEventListener('change', () => {
     Player.volumeSetting();
+    IconAnimation();
 });
 volume.addEventListener('mousemove', () => {
     Player.volumeSetting();
+    IconAnimation();
 });
+speakerIcon.addEventListener("mouseover", () => {
+    volume.style.display = "flex";
+    speakerIcon.style.opacity = "0.5";
+
+});
+speakerIcon.parentElement.addEventListener("mouseleave", () => {
+    volume.style.display = "";
+    speakerIcon.style.opacity = "";
+});
+//functions
+const IconAnimation = () => {
+    if(+volume.value === 0) {
+        speakerIcon.src = "icons/volume/volume0.svg";
+    }
+    else if(+volume.value < 33) {
+        speakerIcon.src = "icons/volume/volume1.svg";
+    }
+    else if(+volume.value < 66) {
+        speakerIcon.src = "icons/volume/volume2.svg";
+    }
+    else {
+        speakerIcon.src = "icons/volume/volume3.svg";
+    }
+};
